30f7cf18bdd430273289c723e2ff15424d6cef4f
ShadowKingAB <128289538+ShadowKingAB@users.noreply.github.com>
2023-11-05 22:04:46 +0530

    updated

--- weathe.js
+++ weathe.js
@@ -16,7 +16,7 @@ async function checkWeather(city){
         var data = await response.json();
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-        document.querySelector(".feels_like").innerHTML =  "Feels like " + data.main.feels_like + "°C";
+        document.querySelector(".feels_like").innerHTML =  "Feels like " + Math.round( data.main.feels_like) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML = data.wind.speed + "kmph";
     
@@ -48,4 +48,12 @@ async function checkWeather(city){
 
 searchBtn.addEventListener("click", ()=>{
     checkWeather(searchBox.value);
+    
 })
+searchBox.addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+        event.preventDefault(); // Prevent form submission
+
+        checkWeather(searchBox.value);
+    }
+});
